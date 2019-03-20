906f2d8825d378e293a549a2d520ff689c569e76
musicmxn <25290201+musicmxn@users.noreply.github.com>
2019-03-19 21:43:54 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+
+    $('.carousel').carousel();
+    initMyChart();
+
+
+
+
+
+
+
+
+
+
+
+})
+
+function initMyChart() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Winter', 'Spring', 'Summer', 'Fall'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.5)',
+                    'rgba(54, 162, 235, 0.5)',
+                    'rgba(255, 206, 86, 0.5)',
+                    'rgba(75, 192, 192, 0.5)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
